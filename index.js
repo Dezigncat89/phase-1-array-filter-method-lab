3389b41fb47aea4f37549d36e2b1b8b6bad36055
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-03-23 15:12:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+beforeEach(function () {
+      drivers.length = 0;
+
+//Uses Filter Method Retrive Values in Array
+function findMatching(result) {
+  return result = drivers.filter(aName => aName.includes('Bobby', 'Sammy'));
+}
+
+console.log(result);
+
+// Use Filter Method to Retrive Beginning Provided Letters  
+function fuzzyMatch(resultTwo) {
+  return resultTwo = drivers.filter(names => names.includes('Sa'));
+}
+
+console.log(resultTwo);
+
+//Filter Method to Retrive Objects in Array
+function matchName(resultThree) {
+  return resultThree = drivers.filter((persons) => persons.name === 'Bobby')
+}
+
+console.log(resultThree);
+ 
+
+
+
